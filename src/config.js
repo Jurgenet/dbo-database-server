8bb7d71240f96f3e25e3a19161c8608422584708
Yury Ivanchenko <jurgenet@gmail.com>
2023-01-23 20:00:49 +0300

    rename var dbLocalConnectionUrl -> dbConnectionUrl

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports.port = process.env.PORT
 module.exports.serverName = process.env.SERVER_NAME
-module.exports.dbLocalConnectionUrl = process.env.MONGODB_CONNECTION
+module.exports.dbConnectionUrl = process.env.MONGODB_CONNECTION
 module.exports.dbToolsConnectionUrl = process.env.MONGODB_TOOLS_CONNECTION
 module.exports.dbBackupFolder = process.env.MONGODB_BACKUP_FOLDER
 module.exports.dbDumpPath = process.env.DUMP
